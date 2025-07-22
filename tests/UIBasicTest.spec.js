eeec10306bcf02259e25989d84f6a86c95463106
Abdullah Al Faroque <rfnshare@gmail.com>
2025-07-22 20:58:39 +0600

    Handling UI Components(Dropdowns ,Radio Buttons, Childwindows) with Playwright

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -34,7 +34,7 @@ test('First PlayWright Test Using Page', async ({page})=>
     await expect(page).toHaveTitle("Google");
 });
 
-test.only("Third Playwright Test", async ({page})=>
+test("Third Playwright Test", async ({page})=>
 {
    const randomNumber = Math.floor(Math.random()*1000000);
    const email = `testuser${randomNumber}@example.com`;
@@ -74,3 +74,49 @@ test.only("Third Playwright Test", async ({page})=>
    console.log(await page.locator("[class='card-body'] b").allTextContents());
    console.log(email);
 });
+
+test('UI Control Testing', async ({page})=>
+{
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    console.log(await page.title());
+    const userName = page.locator('#username');
+    const passWord = page.locator("[type='password']");
+    await userName.fill("rahulshettyacademy");
+    await passWord.fill("learning");
+    const dropdwon =  page.locator("select.form-control");
+    await dropdwon.selectOption("consult");
+    await page.locator("[class='checkmark']").last().click();
+    await page.locator("#okayBtn").click();
+    console.log(`Is Radio Button Clicked? ${await page.locator("[class='checkmark']").last().isChecked()}`);
+    await expect(page.locator("[class='checkmark']").last()).toBeChecked();
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    console.log(`Is terms checkbox checked? ${await page.locator("#terms").isChecked()}`);
+    expect(await page.locator("#terms").isChecked()).toBeFalsy();
+    const documentLocator = page.locator(".blinkingText");
+    await expect(documentLocator).toHaveAttribute('class', 'blinkingText');
+    // await page.pause();
+    // await signIn.click();
+});
+
+test.only("Child Window", async ({browser})=>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const documentLocator = page.locator(".blinkingText");
+    const [newPage] = await Promise.all([
+    context.waitForEvent('page'),
+    documentLocator.click(),
+    ])
+    const text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@");
+    const domain = arrayText[1].split(" ")[0];
+    console.log(domain);
+    expect(domain).toBe("rahulshettyacademy.com");
+    await page.locator("#username").fill(domain);
+    // await page.pause();
+    const tt = await page.locator("#username").inputValue();
+    console.log(`Hello, ${tt}`);
+})

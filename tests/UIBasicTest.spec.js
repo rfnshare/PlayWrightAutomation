4c5336271067050a77666e73ba34f746d481c287
Abdullah Al Faroque <rfnshare@gmail.com>
2025-07-22 15:24:14 +0600

    create e2e for client app

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -1,20 +1,30 @@
 // const {test} = require('@playwright/test');
 import { expect, test } from '@playwright/test';
 
-test.only('First PlayWright Test Using Context', async ({browser})=>
+test('First PlayWright Test Using Context', async ({browser})=>
 {
+
+
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
     console.log(await page.title());
-    await page.locator('#username').fill("");
-    await page.locator("[type='password']").fill("learning");
-    await page.locator('#signInBtn').click();
-    // console.log(await page.locator("[class*=alert]").textContent());
+    const userName = page.locator('#username');
+    const passWord = page.locator("[type='password']");
+    const signIn = page.locator('#signInBtn');
+    await userName.fill("rahulshetty");
+    await passWord.fill("learning");
+    await signIn.click();
     await expect(page.locator("[class*=alert]")).toContainText("Incorrect");
-    // await page.waitForTimeout(5000);
 
-
+    await userName.fill("rahulshettyacademy");
+    await passWord.fill("learning");
+    await signIn.click();
+    const product = page.locator("[class=card-title] a");
+    // console.log(await product.first().textContent());
+    // console.log(await product.nth(1).textContent());
+    // console.log(await product.last().textContent());
+    console.log(await product.allTextContents())
 });
 
 test('First PlayWright Test Using Page', async ({page})=>
@@ -22,4 +32,43 @@ test('First PlayWright Test Using Page', async ({page})=>
     await page.goto('https://google.com');
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
+});
+
+test.only("Third Playwright Test", async ({page})=>
+{
+   const randomNumber = Math.floor(Math.random()*1000000);
+   const email = `testuser${randomNumber}@example.com`;
+   await page.goto("https://rahulshettyacademy.com/client");
+   const userEmail = page.locator("#userEmail");
+   const userPassword = page.locator("#userPassword");
+   const regBtn = page.locator(".text-reset");
+   await regBtn.click();
+
+   const firstName = page.locator("#firstName");
+   const lastName = page.locator("#lastName");
+   const regUserEmail = page.locator("#userEmail");
+   const userMobile = page.locator("#userMobile");
+   const regUserPassword = page.locator("#userPassword");
+   const confirmPassword = page.locator("#confirmPassword");
+   const checkbox = page.locator('[type="checkbox"]');
+   const confirmBtn = page.locator('[type="submit"]');
+
+   await firstName.fill("Abdullah Al");
+   await lastName.fill("Faroque");
+   await regUserEmail.fill(email);
+   await userMobile.fill("1111111111");
+   await regUserPassword.fill("Rfns@0000");
+   await confirmPassword.fill("Rfns@0000");
+   await checkbox.click();
+   await confirmBtn.click();
+
+   await expect(page.locator("[class='headcolor']")).toContainText("Successfully");
+   await page.locator("[routerlink='/auth']").click();
+   await userEmail.fill(email);
+   await userPassword.fill("Rfns@0000");
+   await confirmBtn.click();
+
+   console.log(await page.locator("[class='card-body'] b").first().textContent());
+   console.log(await page.locator("[class='card-body'] b").allTextContents());
+   console.log(await email);
 });
